79b7eb2c52ceaf7e0a1786b324bfde4fabaa789b
renanm <renan@renanmedeiros.net>
2021-11-02 17:16:03 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'renan@renanmedeiros.net';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'renan@renanmedeiros.net';
